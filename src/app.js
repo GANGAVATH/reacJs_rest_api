cdd7051068d5587e0469a1a788571ddefee79948
stackmern@gmail.com <stackmern@gmail.com>
2019-04-10 00:07:12 +0530

    react rest api

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import React from  "react";
+import ReactDOM from 'react-dom';
+import { Route,Link ,NavLink} from 'react-router-dom'
+
+const Homepage =()=>{
+    return (
+        <h1>Home page </h1>
+    )
+}
+const AboutPage =()=>{
+    return (
+        <h1>About page</h1>
+    )
+}
+const Contact =()=>{
+    return (
+        <h1>contact page</h1>
+    )
+}
+const PageNotFound =() =>{
+    return (
+        <h1> page not found from  </h1>
+    )
+}
+
+const RouterFull =() =>{
+
+}
